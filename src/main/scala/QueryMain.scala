b8653cea31df2a83bca28b7d7eccd4f7a763238d
trillaura <laura.trivelloni@gmail.com>
2018-05-17 15:47:59 +0200

    fix query1

--- src/main/scala/QueryMain.scala
+++ src/main/scala/QueryMain.scala
@@ -1,21 +1,22 @@
-
-object QueryMain {
-
-  def main(args: Array[String]): Unit = {
-
-    if (args.length == 0) {
-      printf("Usage: QueryMain <query_number>\n")
-      return
-    }
-
-    val n = args(0).toInt
-
-    n match {
-      case 1 => Query1.execute()
-      case 2 => Query2.execute()
-      case 3 => Query3.execute()
-    }
-
-  }
-
-}
+//import utils.ProfilingTime
+//
+//object QueryMain {
+//
+//  def main(args: Array[String]): Unit = {
+//
+//    if (args.length == 0) {
+//      printf("Usage: QueryMain <query_number>\n")
+//      return
+//    }
+//
+//    val n = args(0).toInt
+//
+//    n match {
+//      case 1 => ProfilingTime.time{Query1.execute()}
+//      case 2 => ProfilingTime.time{Query2.execute()}
+//      case 3 => ProfilingTime.time{Query3.execute()}
+//    }
+//
+//  }
+//
+//}
