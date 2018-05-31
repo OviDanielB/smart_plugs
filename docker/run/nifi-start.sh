915e519d309be27a16db42ab9f249c89e76b268a
Emanuele Vannacci <emanuele.vannacci@gmail.com>
2018-05-31 19:37:47 +0200

    modify nifi image for deployment

--- docker/run/nifi-start.sh
+++ docker/run/nifi-start.sh
@@ -3,10 +3,11 @@
 HOSTNAME=nifi
 HOST=localhost
 PORT=9999
+HDFS_DEFAULT_FS=hdfs://master:54310
 JAR=1.1.32/nifi-deploy-config-1.1.32.jar
 
 docker network create --driver bridge hadoop_network
-docker run -t -i -p $PORT:$PORT -e NIFI_WEB_HTTP_PORT="$PORT" --network=hadoop_network -d --hostname=$HOSTNAME --name=nifi zanna94/nifi
+docker run -t -i -p $PORT:$PORT -e NIFI_WEB_HTTP_PORT="$PORT" -e HDFS_DEFAULTS_FS="$HDFS_DEFAULT_FS" --network=hadoop_network -d --hostname=$HOSTNAME --name=nifi zanna94/nifi
 
 docker exec nifi bash -c "cd / && curl -O https://raw.githubusercontent.com/trillaura/smart_plugs/master/docker/build/NiFi/template.xml"
 
