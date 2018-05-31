94facbd9285bd1eafacf6b42ddbe98dfa227e117
Emanuele Vannacci <emanuele.vannacci@gmail.com>
2018-05-31 14:32:55 +0200

    modified nifi startup script

--- docker/run/nifi-start.sh
+++ docker/run/nifi-start.sh
@@ -8,6 +8,8 @@ JAR=1.1.32/nifi-deploy-config-1.1.32.jar
 docker network create --driver bridge hadoop_network
 docker run -t -i -p $PORT:$PORT -e NIFI_WEB_HTTP_PORT="$PORT" --network=hadoop_network -d --hostname=$HOSTNAME --name=nifi zanna94/nifi
 
+docker exec nifi bash -c "cd / && curl -O https://raw.githubusercontent.com/trillaura/smart_plugs/master/docker/build/NiFi/template.xml"
+
 printf 'waiting for NiFi...\n'
 until $(curl --output /dev/null --silent --head --fail http://$HOST:$PORT); do
     printf '.'
